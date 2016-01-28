f6becf757637459cdecbf640246dc781390ac583
Haversham94 <r.aguidissou.13@groupe-esigelec.fr>
2016-01-28 17:27:51 +0100

    correction bug account module

--- client/app/account/account.module.js
+++ client/app/account/account.module.js
@@ -5,77 +5,65 @@
 var accountApp = angular.module('accountApp', ['commonApp']);
 //module configuration with differents states(routes) 
 accountApp.config(function ($stateProvider) {
-            $stateProvider
+    $stateProvider
 
-                .state('login', {
-                    url: '/login',
-                    templateUrl: 'app/account/views/login.html',
-                    controller: 'LoginController'
-                })
-                .state('logout', {
-                    url: '/logout?referrer', //referrer -> current user to disconnect
-                    referrer: 'main',
-                    template: '',
-                    controller: function ($state, Auth) {
-                        var referrer = $state.params.referrer ||
-                            $state.current.referrer ||
-                            'main';
-                        Auth.logout(); //calling logout function 
-                        $state.go(referrer); //go to main page after disconnection
-                    }
-                })
+        .state('login', {
+            url: '/login',
+            templateUrl: 'app/account/views/login.html',
+            controller: 'LoginController'
+        })
+        .state('logout', {
+            url: '/logout?referrer', //referrer -> current user to disconnect
+            referrer: 'main',
+            template: '',
+            controller: function ($state, Auth) {
+                var referrer = $state.params.referrer ||
+                    $state.current.referrer ||
+                    'main';
+                Auth.logout(); //calling logout function 
+                $state.go(referrer); //go to main page after disconnection
+            }
+        })
 
-            .state('signup', {
-                    url: '/signup',
-                    templateUrl: 'app/account/views/signup.html',
-                    controller: 'SignupController',
-                })
-                .state('settings', {
-                    url: '/settings',
-                    templateUrl: 'app/account/views/settings.html',
-                    controller: 'SettingsController',
-                    authenticate: true
-                })
+    .state('signup', {
+            url: '/signup',
+            templateUrl: 'app/account/views/signup.html',
+            controller: 'SignupController'
+        })
+        .state('settings', {
+            url: '/settings',
+            templateUrl: 'app/account/views/settings.html',
+            controller: 'SettingsController',
+            authenticate: true
+        })
 
-            .state('profil', {
-                url: '/profil',
-                templateUrl: 'app/account/views/profil.html',
-                controller: 'SettingsController',
-                authenticate: true
-            })
+    .state('profil', {
+        url: '/profil',
+        templateUrl: 'app/account/views/profil.html',
+        controller: 'SettingsController',
+        authenticate: true
+    })
 
-            .state('Retour2', {
-                    url: '/Retour2',
-                    templateUrl: 'app/account/views/profil.html',
-                    controller: 'SettingsController',
-                    authenticate: 'true'
-                })
-                .state('startgame', {
-                    url: '/startgame',
-                    templateUrl: 'app/account/views/jouer.html',
-                    controller: 'SettingsController',
+    .state('backtoProfil', {
+        url: '/profil',
+        templateUrl: 'app/account/views/profil.html',
+        controller: 'SettingsController',
+        authenticate: true
+    })
 
 
-                    .state('backtoProfil', {
-                        url: '/profil',
-                        templateUrl: 'app/account/views/profil.html',
-                        controller: 'SettingsController',
-                        authenticate: true
-                    })
+});
 
 
-                });
+/*block to set up listeners for routing events or unauthenticated requests
+function that validates that we have an authenticated user every time that we change our route
+*/
 
-
-            /*block to set up listeners for routing events or unauthenticated requests
-            function that validates that we have an authenticated user every time that we change our route
-            */
-
-            //definition of $stateChangeStart ==> /client/componenents/auth/router.decorator.js
-            accountApp.run(function ($rootScope) {
-                $rootScope.$on('$stateChangeStart', function (event, next, nextParams, current) {
-                    if (next.name === 'logout' && current && current.name && !current.authenticate) {
-                        next.referrer = current.name;
-                    }
-                });
-            });
+//definition of $stateChangeStart ==> /client/componenents/auth/router.decorator.js
+accountApp.run(function ($rootScope) {
+    $rootScope.$on('$stateChangeStart', function (event, next, nextParams, current) {
+        if (next.name === 'logout' && current && current.name && !current.authenticate) {
+            next.referrer = current.name;
+        }
+    });
+});
